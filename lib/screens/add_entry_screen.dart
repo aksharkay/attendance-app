b4d998633b6014f1238e47ce7a2a684d93ea9d16
Akshar Kommajosyula <aksharkomm@gmail.com>
2021-05-26 22:55:48 +0530

    created attendance scan screen

---
+++ lib/screens/add_entry_screen.dart
@@ -0,0 +1,240 @@
+// A screen that allows users to take a picture using a given camera.
+import 'dart:async';
+import 'dart:io';
+
+import '../widgets/auth_action_button.dart';
+import '../widgets/face_painter.dart';
+import '../widgets/camera_header.dart';
+import '../services/camera_service.dart';
+import '../services/facenet_service.dart';
+import '../services/ml_vision_service.dart';
+import 'package:camera/camera.dart';
+import 'package:firebase_ml_vision/firebase_ml_vision.dart';
+import 'package:flutter/material.dart';
+import 'dart:math' as math;
+
+class AddEntryScreen extends StatefulWidget {
+  final CameraDescription cameraDescription;
+
+  const AddEntryScreen({
+    Key key,
+    @required this.cameraDescription,
+  }) : super(key: key);
+
+  @override
+  AddEntryScreenState createState() => AddEntryScreenState();
+}
+
+class AddEntryScreenState extends State<AddEntryScreen> {
+  /// Service injection
+  CameraService _cameraService = CameraService();
+  MLVisionService _mlVisionService = MLVisionService();
+  FaceNetService _faceNetService = FaceNetService();
+
+  Future _initializeControllerFuture;
+
+  bool cameraInitializated = false;
+  bool _detectingFaces = false;
+  bool pictureTaken = false;
+
+  // switches when the user press the camera
+  bool _saving = false;
+  bool _bottomSheetVisible = false;
+
+  String imagePath;
+  Size imageSize;
+  Face faceDetected;
+
+  @override
+  void initState() {
+    super.initState();
+
+    /// starts the camera & start framing faces
+    _start();
+  }
+
+  @override
+  void dispose() {
+    // Dispose of the controller when the widget is disposed.
+    _cameraService.dispose();
+    super.dispose();
+  }
+
+  /// starts the camera & start framing faces
+  _start() async {
+    await _faceNetService.loadModel();
+    _initializeControllerFuture =
+        _cameraService.startService(widget.cameraDescription);
+    await _initializeControllerFuture;
+
+    setState(() {
+      cameraInitializated = true;
+    });
+
+    _frameFaces();
+  }
+
+  /// draws rectangles when detects faces
+  _frameFaces() {
+    imageSize = _cameraService.getImageSize();
+
+    _cameraService.cameraController.startImageStream((image) async {
+      if (_cameraService.cameraController != null) {
+        // if its currently busy, avoids overprocessing
+        if (_detectingFaces) return;
+
+        _detectingFaces = true;
+
+        try {
+          List<Face> faces = await _mlVisionService.getFacesFromImage(image);
+
+          if (faces != null) {
+            if (faces.length > 0) {
+              // preprocessing the image
+              setState(() {
+                faceDetected = faces[0];
+              });
+
+              if (_saving) {
+                _saving = false;
+                _faceNetService.setCurrentPrediction(image, faceDetected);
+              }
+            } else {
+              setState(() {
+                faceDetected = null;
+              });
+            }
+          }
+
+          _detectingFaces = false;
+        } catch (e) {
+          print(e);
+          _detectingFaces = false;
+        }
+      }
+    });
+  }
+
+  /// handles the button pressed event
+  Future<bool> onShot() async {
+    if (faceDetected == null) {
+      showDialog(
+        context: context,
+        builder: (context) {
+          return AlertDialog(
+            content: Text('No face detected!'),
+          );
+        },
+      );
+
+      return false;
+    } else {
+      _saving = true;
+
+      await Future.delayed(Duration(milliseconds: 500));
+      await _cameraService.cameraController.stopImageStream();
+      await Future.delayed(Duration(milliseconds: 200));
+      XFile file = await _cameraService.takePicture();
+
+      setState(() {
+        _bottomSheetVisible = true;
+        pictureTaken = true;
+        imagePath = file.path;
+      });
+
+      return true;
+    }
+  }
+
+  _onBackPressed() {
+    Navigator.of(context).pop();
+  }
+
+  _reload() {
+    setState(() {
+      _bottomSheetVisible = false;
+      cameraInitializated = false;
+      pictureTaken = false;
+    });
+    this._start();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    final double mirror = math.pi;
+    final width = MediaQuery.of(context).size.width;
+    final height = MediaQuery.of(context).size.height;
+    return Scaffold(
+      body: Stack(
+        children: [
+          FutureBuilder<void>(
+              future: _initializeControllerFuture,
+              builder: (context, snapshot) {
+                if (snapshot.connectionState == ConnectionState.done) {
+                  if (pictureTaken) {
+                    return Container(
+                      width: width,
+                      height: height,
+                      child: Transform(
+                          alignment: Alignment.center,
+                          child: FittedBox(
+                            fit: BoxFit.cover,
+                            child: Image.file(File(imagePath)),
+                          ),
+                          transform: Matrix4.rotationY(mirror)),
+                    );
+                  } else {
+                    return Transform.scale(
+                      scale: 1.0,
+                      child: AspectRatio(
+                        aspectRatio: MediaQuery.of(context).size.aspectRatio,
+                        child: OverflowBox(
+                          alignment: Alignment.center,
+                          child: FittedBox(
+                            fit: BoxFit.fitHeight,
+                            child: Container(
+                              width: width,
+                              height: width *
+                                  _cameraService
+                                      .cameraController.value.aspectRatio,
+                              child: Stack(
+                                fit: StackFit.expand,
+                                children: <Widget>[
+                                  CameraPreview(
+                                      _cameraService.cameraController),
+                                  CustomPaint(
+                                    painter: FacePainter(
+                                        face: faceDetected,
+                                        imageSize: imageSize),
+                                  )
+                                ],
+                              ),
+                            ),
+                          ),
+                        ),
+                      ),
+                    );
+                  }
+                } else {
+                  return Center(child: CircularProgressIndicator());
+                }
+              }),
+          CameraHeader(
+            "ADD ENTRY",
+            onBackPressed: _onBackPressed,
+            // cameraDescription: cameraDescription,
+          ),
+        ],
+      ),
+      floatingActionButtonLocation: FloatingActionButtonLocation.centerFloat,
+      floatingActionButton: !_bottomSheetVisible
+          ? AuthActionButton(
+              _initializeControllerFuture,
+              onPressed: onShot,
+              isLogin: true,
+              reload: _reload,
+            )
+          : Container(),
+    );
+  }
+}
