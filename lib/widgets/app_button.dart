1f4b6239826c077461cd4c5e807cc88bf3acc14b
Akshar Kommajosyula <aksharkomm@gmail.com>
2021-05-27 16:32:45 +0530

    change button color

--- lib/widgets/app_button.dart
+++ lib/widgets/app_button.dart
@@ -4,7 +4,6 @@ class AppButton extends StatelessWidget {
   AppButton(
       {this.onPressed,
       this.text,
-      this.color = const Color(0xF006B1B),
       this.icon = const Icon(
         Icons.add,
         color: Colors.white,
@@ -12,7 +11,6 @@ class AppButton extends StatelessWidget {
   final Function onPressed;
   final String text;
   final Icon icon;
-  final Color color;
   @override
   Widget build(BuildContext context) {
     return InkWell(
@@ -20,11 +18,10 @@ class AppButton extends StatelessWidget {
       child: Container(
         decoration: BoxDecoration(
           borderRadius: BorderRadius.circular(10),
-          color: color,
+          color: Theme.of(context).primaryColor,
           boxShadow: <BoxShadow>[
             BoxShadow(
               color: Theme.of(context).primaryColor,
-              blurRadius: 1,
               offset: Offset(0, 2),
             ),
           ],
