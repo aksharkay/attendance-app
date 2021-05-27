68e8f4daef198a3feceb04d0e501b584da48962e
Akshar Kommajosyula <aksharkomm@gmail.com>
2021-05-27 12:50:00 +0530

    logging data

--- lib/db/database.dart
+++ lib/db/database.dart
@@ -24,6 +24,7 @@ class DataBaseService {
   Future loadDB() async {
     var tempDir = await getApplicationDocumentsDirectory();
     String _embPath = tempDir.path + '/emb.json';
+    print(_embPath);
 
     jsonFile = new File(_embPath);
 
