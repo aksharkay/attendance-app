49eafe27aa0b6cbb0bf571c6ee1dce2ff12ea73e
Akshar Kommajosyula <aksharkomm@gmail.com>
2021-05-28 13:01:53 +0530

    wrapped future builder with expanded widget

--- lib/screens/all_users_screen.dart
+++ lib/screens/all_users_screen.dart
@@ -24,26 +24,28 @@ class _AllUsersScreenState extends State<AllUsersScreen> {
       body: Column(
         children: [
           Text('Double Tap On User To Mark As Present'),
-          FutureBuilder(
-            future: _getAllUsers(context),
-            builder: (ctx, snapshot) {
-              switch (snapshot.connectionState) {
-                case ConnectionState.done:
-                  return Consumer<Entries>(
-                    builder: (context, users, child) {
-                      // print(users.allUsers);
-                      return users.allUsers.length == 0
-                          ? _buildNoUsers()
-                          : _buildListView(users);
-                    },
-                  );
+          Expanded(
+            child: FutureBuilder(
+              future: _getAllUsers(context),
+              builder: (ctx, snapshot) {
+                switch (snapshot.connectionState) {
+                  case ConnectionState.done:
+                    return Consumer<Entries>(
+                      builder: (context, users, child) {
+                        // print(users.allUsers);
+                        return users.allUsers.length == 0
+                            ? _buildNoUsers()
+                            : _buildListView(users);
+                      },
+                    );
 
-                default:
-                  return Center(
-                    child: LinearProgressIndicator(),
-                  );
-              }
-            },
+                  default:
+                    return Center(
+                      child: LinearProgressIndicator(),
+                    );
+                }
+              },
+            ),
           ),
         ],
       ),
